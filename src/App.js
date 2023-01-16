66fc7bad25029494bbdcc1e3b3c502e9cb465c65
BATTHALA VINOD KUMAR <63972486+vinodkumar2704@users.noreply.github.com>
2023-01-16 22:06:39 +0530

    created bakery homepage

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import Header from './components/Header';
+import Main from './components/Main';
+import Cart from './components/Cart';
+import data from './data';
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from 'react';
+function App() {
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
+
+  const onDelete = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty > 0) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } 
+    
+
+  };
+
+  return (
+    <div className="App">
+      <Header countCartItems={cartItems.length}></Header>
+      <div className="row">
+        <Main products={products} onAdd={onAdd} onDelete={onDelete} ></Main>
+        <Cart
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+        ></Cart>
+      </div>
+    </div>
+  );
+}
+
+export default App;
